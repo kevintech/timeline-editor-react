71bc95df575b8a133d3d9eee2e8e94fe7febd8f8
Kevin Herrarte <hello@kevintech.ninja>
2018-04-03 23:38:19 -0600

    Add webpack optimization chunks

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,25 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					chunks: "initial",
+					minChunks: 2,
+					maxInitialRequests: 5, // The default limit is too small to showcase the effect
+					minSize: 1 // This is example is too small to create commons chunks
+				},
+				vendor: {
+					test: /node_modules/,
+					chunks: "initial",
+					name: "vendor",
+					priority: 10,
+					enforce: true
+				}
+			}
+		}
+	},
     module: {
         rules: [
             {
