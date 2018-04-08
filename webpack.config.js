b955c73ee9f626d9ff5c7a5a370a0d8fdcd89586
Kevin Herrarte <hello@kevintech.ninja>
2018-04-07 19:11:28 -0600

    Add npmignore. Change compiled main on package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: "development",
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         libraryTarget: 'commonjs2'
     },
